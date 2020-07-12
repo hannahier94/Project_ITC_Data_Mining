bd91ecf01fc3b2bcda88caa34ae3ee07bef99aee
SebKleiner <31596147+SebKleiner@users.noreply.github.com>
2020-07-12 15:15:03 +0300

    Delete CONSTANTS.py

--- CONSTANTS.py
+++
@@ -1,6 +0,0 @@
-#URLS = ['MachineLearning', 'datascience', 'cscareerquestions', 'DataScienceJobs', 'bigdata', 'Python',
-#        'dataisbeautiful', 'datascienceproject', 'learnmachinelearning', 'analytics', 'artificial', 'dataengineering']
-URLS = ['MachineLearning']
-
-KEYS = ['domain_tag', 'title', 'awards', 'author', 'author_info', 'comments', 'scorelikes', 'scoredislikes', 'dates',
-        'thread', 'spoilers', 'promoted', 'crossposts', 'postype', 'titlescore']
